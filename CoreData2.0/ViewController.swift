c0aed1b83e6c35a49cd139fa7e4cd0f98ff63bf9
RahulM1987 <rahulmorade@winjit.local>
2016-04-11 14:51:23 +0530

    Initial Commit

---
+++ CoreData2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreData2.0
+//
+//  Created by Rahul Morade on 11/04/16.
+//  Copyright © 2016 Rahul Morade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
