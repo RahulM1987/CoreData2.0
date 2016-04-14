05ea067b3ffc1e947dce2919e8a1c358120c208b
RahulM1987 <rahulmorade@vishal-niles-mac-mini.local>
2016-04-14 18:13:15 +0530

    commit

--- CoreData2.0/ViewController.swift
+++ CoreData2.0/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController ,UITableViewDataSource {
+
+    var names = [String]()
+    var people = [NSManagedObject]()
+
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,127 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var Add: UIButton!
+    @IBAction func Addentry(sender: AnyObject)
+    {
+        
+        print("Add entry")
+        let alert = UIAlertController(title: "New Name", message: "Add the name", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        
+        let saveAction = UIAlertAction(title: "Save", style: .Default, handler:
+            { (action:UIAlertAction) -> Void in
+        let textfieldS = alert.textFields?.first
+            self.saveName(textfieldS!.text!)
+            self.tableView.reloadData()
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+            style: .Default) { (action: UIAlertAction) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField) -> Void in
+        }
 
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+
+        presentViewController(alert, animated: true, completion: nil)
+        
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")
+       
+        let person = people[indexPath.row]
+        
+        cell!.textLabel!.text =
+            person.valueForKey("name") as? String
+//        cell!.textLabel?.text = "Rahul"
+        return cell!
+    }
+    
+    
+    func saveName(name: String) {
+        //1
+        let appDelegate =
+        UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        //2
+        let entity =  NSEntityDescription.entityForName("Person",
+            inManagedObjectContext:managedContext)
+        
+        let person = NSManagedObject(entity: entity!,
+            insertIntoManagedObjectContext: managedContext)
+        
+        //3
+        person.setValue(name, forKey: "name")
+        
+        //4
+        do {
+            try managedContext.save()
+            //5
+            people.append(person)
+        } catch let error as NSError  {
+            print("Could not save \(error), \(error.userInfo)")
+        }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //1
+        let appDelegate =
+        UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let managedContext = appDelegate.managedObjectContext
+        
+        //2
+        let fetchRequest = NSFetchRequest(entityName: "Person")
+        
+        //3
+        do {
+            let results =
+            try managedContext.executeFetchRequest(fetchRequest)
+            people = results as! [NSManagedObject]
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+    }
+     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        switch editingStyle {
+        case .Delete:
+            // remove the deleted item from the model
+            let appDel:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+            let context:NSManagedObjectContext = appDel.managedObjectContext
+            context.deleteObject(people[indexPath.row] as NSManagedObject)
+            people.removeAtIndex(indexPath.row)
+            do {
+                try context.save()
+                //5
+            } catch let error as NSError  {
+                print("Could not save \(error), \(error.userInfo)")
+            }
+            //tableView.reloadData()
+            // remove the deleted item from the `UITableView`
+            self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        default:
+            return
+            
+        }
+    }
 }
 
